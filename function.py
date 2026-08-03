f7e74591fa9461c9b4fafe104856666ceb863c7d
zaidmewati23 <zaidmewati126@gmail.com>
2026-08-03 11:30:36 +0530

    I am upload the basics of python in function

---
+++ function.py
@@ -0,0 +1,180 @@
+
+
+
+# def greet(name):
+#     print("mohd",name)
+# greet("zaid")    
+
+# def cal_sum(a,b,c):
+#     sum = a+b+c
+#     print(sum)
+# cal_sum(3,5,1)
+# cal_sum(64,5,9)  
+
+# def multiple(a,b):
+#     mul = a*b
+#     print(mul)
+# multiple(5,3)  
+# multiple(8,3)
+# multiple(15,4)
+# multiple(40,5)  
+
+# def greet(name="mahmaan"):
+#     print("aor kya haal h",name)
+# greet()
+# greet("mohd zaid")    
+
+# def greet(name="mohd  zaid"):
+#     print("hii bro",name)
+# greet()
+# greet("soab  khan")  
+
+# a = int(input("enter the first number:"))
+# b = int(input("enter the second number:"))
+
+
+
+# def add(a,b):
+#     result = a+b
+#     print(result)
+    
+# def sub(a,b):
+#     result = a-b
+#     print(result)
+    
+# def mul(a,b):
+#     result = a*b
+#     print(result)
+# def dev(a,b):
+#     if b == 0:
+#         print("division is not possible")
+#     else:
+#         result = a/b
+#         print(result)
+        
+    
+# # num1 = int(input("enter the first number:"))
+# # num2 = int(input("enter the second number:"))
+
+# # print("addition:",add(num1,num2))
+# # print("multiplication:",mul(num1,num2))
+# # print("subtract:",sub(num1,num2))
+# # print("division:",dev(num1,num2))
+
+# add(a,b)
+# sub(a,b)
+# mul(a,b)
+# dev(a,b)
+
+# a = int(input("enter the first number:"))
+# b = int(input("enter the second number:"))
+
+
+# def add(a,b):
+#     result = a+b
+#     print(result)
+# def sub(a,b):
+#     result = a-b
+#     print(result)
+# def mul(a,b):
+#     result = a*b
+#     print(result)
+# def div(a,b):
+#     if b == 0:
+#         print("division is not possible")
+#     else:
+#         result = a/b
+#         print(result)
+
+# add(a,b)
+# sub(a,b)
+# mul(a,b)
+# div(a,b)
+        
+# num = int(input("enter  the number:"))
+
+# def check_even_odd(num):
+#     if num == 0:
+#         print("it is zero")
+#     elif num % 2 == 0:
+#         print("even",num)
+
+#     else:
+#          print("odd->",num)
+    
+
+# check_even_odd(num)
+# a = int(input("enter the first number:"))
+# b = int(input("enter the second number:"))
+# c = int(input("enter the third number:"))   
+# def largest(a,b,c):
+#     if a>b and a>c:
+#         print("A is largest")
+#     elif b>a and b>c:
+#         print("B is largest")
+#     else:
+#         print("C is largest")        
+# largest(a,b,c)
+
+# n = int(input("enter the number:"))
+
+# def factorial(n):
+#     fact = 1
+#     for i in range(1, n+1):
+#         fact*= i
+#     print(fact)
+# factorial(n)
+
+# n = int(input("enter the number"))
+
+# def prime(n):
+#     if 1<=n:  
+#         return False
+#         for i in range(2,n):
+#             if n%i == 0:
+#                 return False
+#     return True
+# if prime(n):
+#     print("prime number:",n)
+# else:
+#     print("not prime")      
+ 
+# text = str(input("reverse the text:->"))
+# def reverse_string(text):
+#     if 
+#     for text in range[::-1]
+        
+# reverse_string(text)
+
+
+# def reverse_string(text):
+#     return text[::-1]
+
+# print(reverse_string("mohd zaid"))
+
+# r = "java"
+# print(r[::-1])
+
+# def reverse_striing(r):
+#     rev = ""
+#     for i in range(len(r)- 1,- 1,- 1):
+#         rev += r[i]
+#     return rev
+# r = input("enter string..")
+
+
+# def reverse_string(s):
+#     rev = "  "
+#     for i in range(len(s) -1,-1,-1):
+#         rev += s[i]
+#     return rev
+# s = input("entr string...")
+# print("reversed string..",reverse_string(s))    
+
+# def reverse_string(s):
+#     rev = " "
+#     for i in range(len(s) -1,-1,-1,):
+#         rev += s[i]
+#     return rev
+# s = input("enter the string...")
+# print("reversed string..",reverse_string(s))    
